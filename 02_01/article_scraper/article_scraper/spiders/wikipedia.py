b8472aa45823dde1a4af0b216d707f35b5d91ded
NormundsB <92847485+NormundsB@users.noreply.github.com>
2022-06-05 20:22:39 +0300

    ss

--- 02_01/article_scraper/article_scraper/spiders/wikipedia.py
+++ 02_01/article_scraper/article_scraper/spiders/wikipedia.py
@@ -3,11 +3,13 @@ import scrapy
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
+
 class WikipediaSpider(CrawlSpider):
     name = 'wikipedia'
     allowed_domains = ['en.wikipedia.org']
     start_urls = ['https://en.wikipedia.org/wiki/Kevin_Bacon']
-    rules = [Rule(LinkExtractor(allow=r'wiki/((?!:).)*$'), callback='parse_info', follow=True)]
+    rules = [Rule(LinkExtractor(allow=r'wiki/((?!:).)*$'),
+                  callback='parse_info', follow=True)]
 
     def parse_info(self, response):
         return {
